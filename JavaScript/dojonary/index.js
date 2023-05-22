ca60a4d232bc5f580eb0e359c639203e0f7a9aee
trrez <tatianagutierrez15@gmail.com>
2023-05-22 17:44:28 -0400

    terminado

---
+++ JavaScript/dojonary/index.js
@@ -0,0 +1,11 @@
+function changeName(element) {
+    element.innerText = 'Logout'
+}
+
+function deleteButton(element) {
+    element.remove()
+}
+
+function wasLiked(element) {
+    alert("Ninja was liked")
+}
