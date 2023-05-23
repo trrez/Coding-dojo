cafdce21cf55ce634ae5a631a85f0c2815d546d9
trrez <tatianagutierrez15@gmail.com>
2023-05-22 23:44:00 -0400

    done

--- JavaScript/me_gusta/index.js
+++ JavaScript/me_gusta/index.js
@@ -1,18 +1,17 @@
-let count1 = 3;
-let count2 = 12;
-let count3 = 9;
-
 function add1(){
+    let count1 = 3;
     count1 ++
     document.getElementById("like-1").innerHTML = `${count1} like(s)`
 }
 
 function add2(){
+    let count2 = 12;
     count2 ++
     document.getElementById("like-2").innerHTML = `${count2} like(s)`
 }
 
 function add3(){
+    let count3 = 9;
     count3 ++
     document.getElementById("like-3").innerHTML = `${count3} like(s)`
 }
