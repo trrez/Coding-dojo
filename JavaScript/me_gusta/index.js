c82edb7d6cbed8175ffe1adf2a2e758a0782e7fa
trrez <tatianagutierrez15@gmail.com>
2023-05-22 23:42:36 -0400

    done

---
+++ JavaScript/me_gusta/index.js
@@ -0,0 +1,19 @@
+let count1 = 3;
+let count2 = 12;
+let count3 = 9;
+
+function add1(){
+    count1 ++
+    document.getElementById("like-1").innerHTML = `${count1} like(s)`
+}
+
+function add2(){
+    count2 ++
+    document.getElementById("like-2").innerHTML = `${count2} like(s)`
+}
+
+function add3(){
+    count3 ++
+    document.getElementById("like-3").innerHTML = `${count3} like(s)`
+}
+
