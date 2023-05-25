79a0574140928ef2cc11a88aec6bc19ef0071b04
trrez <tatianagutierrez15@gmail.com>
2023-05-24 21:17:28 -0400

    done

--- JavaScript/calculadora/index.js
+++ JavaScript/calculadora/index.js
@@ -1,19 +1,46 @@
-var displayDiv = document.querySelector("#display");
+const display = document.getElementById("display");
+let num1 = "";
+let num2 = "";
+let op = "";
 
-function press(num){
-  numero = num
-  displayDiv.innerText += num
-  return num
+
+function press(num) {
+    num1 += num;
+    display.innerHTML = num1;
 }
 
-function setOP(op){
-  displayDiv.innerText += op
+function setOP(key) {
+    op = key;
+    num2 = num1;
+    num1 = "";
 }
 
-function clr(){
-  displayDiv.innerText = ''
+function clr() {
+    num1 = "";
+    num2 = "";
+    op = "";
+    display.innerHTML = "0";
 }
 
-function calculate(){
+function calculate() {
+    let a = parseFloat(num2);
+    let b = parseFloat(num1);
+    let res = 0;
+    switch(op) {
+        case "+":
+        res = a + b;
+        break;
+        case "-":
+        res = a - b;
+        break;
+        case "*":
+        res = a * b;
+        break;
+        case "/":
+        res = a / b;
+        break;
+    }
+    num1 = res;
+    op = "";
+    display.innerHTML = res;
 }
-
