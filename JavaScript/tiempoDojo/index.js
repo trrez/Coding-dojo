26a15386cd588c9eb842a39748817c222743d8a4
trrez <tatianagutierrez15@gmail.com>
2023-05-29 18:28:26 -0400

    done

--- JavaScript/tiempoDojo/index.js
+++ JavaScript/tiempoDojo/index.js
@@ -1,14 +1,6 @@
 const cookie = document.getElementById('cookie');
 const btn = document.getElementById('button');
-const city = document.getElementById('show-city');
-const temp = document.getElementById('temp');
-const temp1 = document.getElementById('temp1');
-const temp2 = document.getElementById('temp2');
-const temp3 = document.getElementById('temp3');
-const tempLow = document.getElementById('temp-low');
-const tempLow1 = document.getElementById('temp-low1');
-const tempLow2 = document.getElementById('temp-low2');
-const tempLow3 = document.getElementById('temp-low3');
+
 
 btn.addEventListener('click', hideCookie)
 
@@ -16,19 +8,21 @@ function hideCookie(){
     cookie.remove()
 }
 
-city.addEventListener('click', showCity)
-
 function showCity(){
     alert("Loading weather report...")
 }
 
-function changeTemp(){
-    temp.innerText = '75°';
-    temp1.innerText = '80°'
-    temp2.innerText = '69°'
-    temp3.innerText = '78°'
-    tempLow.innerText = '65°';
-    tempLow1.innerText = '66°';
-    tempLow2.innerText = '61°';
-    tempLow3.innerText = '70°';
+function changeTemp(element){
+    let tempetures = document.querySelectorAll('.high, .low')
+    for(let i = 0; i < tempetures.length; i++){
+        let currentTemp = Number(tempetures[i].textContent)
+
+        if(element.value === "celcius"){
+            let celciusResult = (currentTemp - 32) / 1.8
+            tempetures[i].textContent = parseInt(celciusResult) 
+        }else{
+            let farenheitResult = (9/5) * currentTemp + 32
+            tempetures[i].textContent = parseInt(farenheitResult) 
+        }
+    }
 }
